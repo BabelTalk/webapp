fa72089e667412efbb0c57f86b416d650040e5d1
louremipsum <myself.vinayak2@gmail.com>
2024-08-27 19:57:32 +0530

    fix: rm all instances of clerk

--- src/app/(clerk)/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { ClerkProvider } from "@clerk/nextjs";
-
-export default function ClerkAuth({ children }: { children: React.ReactNode }) {
-  return <ClerkProvider>{children}</ClerkProvider>;
-}
---
+++ src/app/talk/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+function Talking({}: Props) {
+  return <div>page</div>;
+}
+
+export default Talking;
