43cc4dcc639a4189d1f2baa91e427853571e77de
louremipsum <myself.vinayak2@gmail.com>
2024-10-16 22:14:21 +0530

    Add Sonner UI component for displaying toasts

--- src/lib/Provider.tsx
+++ src/lib/Provider.tsx
@@ -1,11 +1,13 @@
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import { ThemeProvider } from "next-themes";
+import { Toaster } from "@/components/ui/sonner";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <UserProvider>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         {children}
+        <Toaster richColors />
       </ThemeProvider>
     </UserProvider>
   );
