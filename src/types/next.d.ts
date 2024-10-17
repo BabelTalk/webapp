3acaec1023c5d14ae3c4ce4c447394840e8af5e2
louremipsum <myself.vinayak2@gmail.com>
2024-10-17 08:37:32 +0530

    Refactor NextApiResponseServerIO type definition

--- src/types/next.d.ts
+++ src/types/next.d.ts
@@ -1,10 +1,11 @@
-import { Server as NetServer, Socket } from "net";
+import { Server as HTTPServer } from "http";
+import { Socket } from "net";
 import { NextApiResponse } from "next";
 import { Server as SocketIOServer } from "socket.io";
 
 export type NextApiResponseServerIO = NextApiResponse & {
   socket: Socket & {
-    server: NetServer & {
+    server: HTTPServer & {
       io: SocketIOServer;
     };
   };
